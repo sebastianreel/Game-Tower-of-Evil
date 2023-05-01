0ac5df761d2d0015852c48ab9f0677a6b2e4255b
Sebastian Reel <sebastianspike@gmail.com>
2023-05-01 13:54:25 -0700

    Tutorial and Correct Level Addition
    Level with all types of enemies.
    Also the tutorial gives basics of jumping and starts the game more thematically.

--- Ascendant/Assets/Scripts/FlyingEnemy.cs
+++ Ascendant/Assets/Scripts/FlyingEnemy.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+/*
+Sebastian Reel
+CS 328 - Game Desgin
+*/
+
 public class FlyingEnemy : MonoBehaviour
 {
     public float speed;
---
+++ Ascendant/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/*
+ Sebastian Reel
+ CS 328 - Game Design
+ */
+
+public class NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("LevelOneFromSeb");
+    }
+}
