5f9410bf21881799abf4a639b75ee0567fd2caae
Sebastian Reel <sebastianspike@gmail.com>
2023-05-11 22:17:34 -0700

    SebTest Damage Effect
    created and implemented working damage effect on player taking damage. custom image with blood effect added too

--- Ascendant/Assets/Scripts/PlayerHealth.cs
+++ Ascendant/Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
@@ -9,16 +8,36 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;
     
     public HealthBar healthBar;
+
+    [Header("Damage Overlay")]
+    public Image damageOvelay;
+    public float fadeSpeed;
+    public float duration;
+    private float durationTimer;
+
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        damageOvelay.color = new Color(damageOvelay.color.r, damageOvelay.color.g, damageOvelay.color.b, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(damageOvelay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > duration)
+            {
+                // fade image
+                float tempAlpha = damageOvelay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
 
+                damageOvelay.color = new Color(damageOvelay.color.r, damageOvelay.color.g, damageOvelay.color.b, tempAlpha);
+            }
+        }
     }
 
     public void TakeDamage(int damage){
@@ -27,5 +46,10 @@ public class PlayerHealth : MonoBehaviour
         if(currentHealth <= 0){
             SceneManager.LoadScene("MainMenu");
         }
+
+        durationTimer = 0;
+        damageOvelay.color = new Color(damageOvelay.color.r, damageOvelay.color.g, damageOvelay.color.b, 1);
     }
+
+
 }
